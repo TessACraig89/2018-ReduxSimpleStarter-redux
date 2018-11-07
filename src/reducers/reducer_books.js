489b79e78e582813d945aaf7c2b8b3054d019aaf
TessACraig89 <tesscraig.webdev@gmail.com>
2018-11-07 12:12:08 -0600

    writes books reducer function, which returns a list of titles of books

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+// function that returns a piece of app state, list of books
+function() {
+  returh [
+    { title: "Billy the Kid's Old Time Oddities" },
+    { title: "Dark Engine" },
+    { title: "Barbarella: Red Gospel" },
+    { title: "Tank Girl" }
+  ]
+}
