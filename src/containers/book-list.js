3b57dca75473a7973bf5936f40b8c0926deca72a
TessACraig89 <tesscraig.webdev@gmail.com>
2018-11-13 11:43:11 -0600

    removes export default from BookList component

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-export default class BookList extends Component {
+class BookList extends Component {
   rederList() {
     return this.props.books.map((book) => {
       return (
