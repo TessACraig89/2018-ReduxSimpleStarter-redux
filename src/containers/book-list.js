5f4171e6cec7d706c82a8d1ee4ef442ba68f7ca1
TessACraig89 <tesscraig.webdev@gmail.com>
2018-11-14 19:33:01 -0600

    fixes spelling errors

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class BookList extends Component {
-  rederList() {
+  renderList() {
     return this.props.books.map((book) => {
       return (
-        <li key={book.title}  className="list-group-item">(book.title)</li>
+        <li key={book.title}  className="list-group-item">{book.title}</li>
       );
     });
   }
@@ -14,7 +14,7 @@ class BookList extends Component {
       <ul className="list-group col-sm-4">
         {this.renderList()}
       </ul>
-    );
+    )
   }
 }
 // takes application state, array of books and active book, and whatever is returned will show up as props inside bookList
